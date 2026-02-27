d685b47f14e5eac9ee2a06b6f0cbcd941dcd10ac
Shehma Ahmad <shehmaahmad@cmail.carleton.ca>
2026-02-27 09:56:39 -0500

    Changing files, practicing

---
+++ Scripts/MyScript.R
@@ -0,0 +1,22 @@
+#Shehma Ahmad
+#February 27, 2026
+#File created, staged (ready to commit), committed
+#A = Added
+#Staging the files before committing
+#Hit commit to access the window
+#Initial Commit with gitignore, readme, etc., hit commit, its shows:
+# >>> /usr/bin/git commit -F /var/folders/rw/4dv3y9gd6nx7n8j44kx7rds00000gn/T/Rtmp1pNKoX/git-commit-message-20dc33109323.txt
+# [main (root-commit) 9f20b45] Initial Commit with gitignore, readme, etc.
+# 4 files changed, 22 insertions(+)
+# create mode 100644 .gitignore
+# create mode 100644 GitWorkshop.Rproj
+# create mode 100644 Readme.md
+# create mode 100644 Scripts/MyScript.R
+
+
+#What the symbols represent
+# ? (Yellow): The file is untracked (new and not yet added to Git version control).
+# A (Green): The file has been added to the staging area, ready to be committed.
+# M (Blue): The file has been modified since the last commit.
+# D (Red): The file has been deleted from the project.
+# R (Purple) : Renamed
